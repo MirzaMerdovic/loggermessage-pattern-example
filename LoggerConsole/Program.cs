519776a6c2d6022d556a29a55a9f3ee92944c0a3
mimme <mirzamerdovic@gmail.com>
2021-09-11 22:58:35 +0200

    Load logging configuration from appsettings.json

--- LoggerConsole/Program.cs
+++ LoggerConsole/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.IO;
-using System.Text.Json;
-using Microsoft.Extensions.Configuration;
+﻿using System.Text.Json;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -12,29 +10,19 @@ namespace LoggerConsole
     {
         internal static void Main(string[] args)
         {
-            IConfigurationRoot configuration =
-                new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json")
-                    .AddEnvironmentVariables()
-                    .Build();
-
-            var host = new HostBuilder()
-                .ConfigureAppConfiguration(x =>
-                {
-                    x.AddConfiguration(configuration);
-                })
-                .ConfigureLogging((ctx, builder) =>
-                {
-                    builder.SetMinimumLevel(LogLevel.Debug);
-                    builder.AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Warning);
-                    builder.AddJsonConsole(x =>
-                            {
-                                x.IncludeScopes = true;
-                                x.UseUtcTimestamp = true;
-                                x.JsonWriterOptions = new JsonWriterOptions { Indented = true };
-                            });
-                })
+            var host = Host.CreateDefaultBuilder()
+                // Uncomment if you don't want to use appsettings.json configuration
+                //.ConfigureLogging((ctx, builder) =>
+                //{
+                //    builder.SetMinimumLevel(LogLevel.Debug);
+                //    builder.AddFilter<ConsoleLoggerProvider>("Microsoft", LogLevel.Warning);
+                //    builder.AddJsonConsole(x =>
+                //            {
+                //                x.IncludeScopes = true;
+                //                x.UseUtcTimestamp = true;
+                //                x.JsonWriterOptions = new JsonWriterOptions { Indented = true };
+                //            });
+                //})
                 .ConfigureServices((ctx, builder) =>
                 {
                     builder.AddHostedService<LoggerHost>();
